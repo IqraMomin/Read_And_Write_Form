3c218a2caf92a9ce8f779ec894351d58ae8b2bde
IqraMomin <iqra2018momin@gmail.com>
2026-03-27 03:31:46 +0530

    Write to file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs= require("fs");
 const server = http.createServer((req,res)=>{
     const url =req.url;
     const method = req.method;
@@ -6,11 +7,31 @@ const server = http.createServer((req,res)=>{
         res.setHeader("Content-type","text/html");
         res.end(
             `
-            <form>
+            <form action="/message" method="POST">
             <label>Name:</label>  
             <input tpe="text" name="username"/>  
+            <button type="submit">Add</button>
             </form>
             `
         )
     }
+    else if(req.url==="/message"){
+        let body = [];
+        req.on("data",(chunks)=>{
+            body.push(chunks);
+        });
+        req.on("end",()=>{
+            let buffer = Buffer.concat(body);
+            let formData = buffer.toString();
+            const formValues = formData.split("=")[1];
+            fs.writeFile("formValues.txt",formValues,(err)=>{
+                res.statusCode=302;
+                res.setHeader("Location","/");
+                res.end();
+            })
+        })
+    }
+})
+server.listen(3001,()=>{
+    console.log("server is running");
 })
