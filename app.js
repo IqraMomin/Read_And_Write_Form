8ebe2edf266953685b938e584959691b9c0f1057
IqraMomin <iqra2018momin@gmail.com>
2026-03-28 02:58:44 +0530

    Clean up code

--- app.js
+++ app.js
@@ -1,77 +1,8 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes")
 
-const server = http.createServer((req, res) => {
+routes.anotherFunction();
 
-    if (req.url === "/" && req.method === "POST") {
-
-        let body = [];
-      
-        req.on("data", (chunk) => {
-          body.push(chunk);
-        });
-      
-        req.on("end", () => {
-          const buffer = Buffer.concat(body);
-          const formData = buffer.toString();
-          const value = decodeURIComponent(formData.split("=")[1]);
-      
-          fs.writeFile("formValues.txt", value, () => {
-      
-            // ✅ Read again after writing
-            fs.readFile("formValues.txt", "utf-8", (err, data) => {
-              res.setHeader("Content-Type", "text/html");
-      
-              res.write(`
-                <body>
-                  <p>${data || ""}</p>
-                  <form action="/" method="POST">
-                    <label>Name:</label>  
-                    <input type="text" name="username"/>  
-                    <button type="submit">Add</button>
-                  </form>
-                </body>
-              `);
-      
-              res.end();
-            });
-      
-          });
-        });
-      }
-
-  else if (req.url === "/" && req.method === "POST") {
-
-    let body = [];
-
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    req.on("end", () => {
-      const buffer = Buffer.concat(body);
-      const formData = buffer.toString();
-      const value = decodeURIComponent(formData.split("=")[1]);
-
-      fs.writeFile("formValues.txt", value, () => {
-
-
-        res.setHeader("Content-Type", "text/html");
-
-        res.write(`
-          <body>
-            <p>${value}</p>
-            <form action="/" method="POST">
-              <label>Name:</label>  
-              <input type="text" name="username"/>  
-              <button type="submit">Add</button>
-            </form>
-          </body>
-        `);
-        res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+
+const requestHandler = (req,res)=>{
+    if (req.url === "/" && req.method === "POST") {
+
+        let body = [];
+      
+        req.on("data", (chunk) => {
+          body.push(chunk);
+        });
+      
+        req.on("end", () => {
+          const buffer = Buffer.concat(body);
+          const formData = buffer.toString();
+          const value = decodeURIComponent(formData.split("=")[1]);
+      
+          fs.writeFile("formValues.txt", value, () => {
+      
+            fs.readFile("formValues.txt", "utf-8", (err, data) => {
+              res.setHeader("Content-Type", "text/html");
+      
+              res.write(`
+                <body>
+                  <p>${data || ""}</p>
+                  <form action="/" method="POST">
+                    <label>Name:</label>  
+                    <input type="text" name="username"/>  
+                    <button type="submit">Add</button>
+                  </form>
+                </body>
+              `);
+      
+              res.end();
+            });
+      
+          });
+        });
+      }
+
+  else if (req.url === "/" && req.method === "POST") {
+
+    let body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const buffer = Buffer.concat(body);
+      const formData = buffer.toString();
+      const value = decodeURIComponent(formData.split("=")[1]);
+
+      fs.writeFile("formValues.txt", value, () => {
+
+
+        res.setHeader("Content-Type", "text/html");
+
+        res.write(`
+          <body>
+            <p>${value}</p>
+            <form action="/" method="POST">
+              <label>Name:</label>  
+              <input type="text" name="username"/>  
+              <button type="submit">Add</button>
+            </form>
+          </body>
+        `);
+        res.end();
+      });
+    });
+  }
+}
+
+const anotherFunction = ()=>{
+    console.log("Another Function");
+}
+
+module.exports = {
+    requestHandler,
+    anotherFunction
+};
